aaf673b55770b4d7352f8a97a213289ba2ae8e33
IslamRamzy444 <islamramzy2@gmail.com>
2025-07-07 09:52:45 +0300

    Finish splash screen and initiating the basic localization settings to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:project_2_evently/ui/home/home_screen.dart';
 import 'package:project_2_evently/utils/app_routes.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:project_2_evently/utils/app_theme.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -12,11 +13,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.homeScreenRouteName,
       routes: {
         AppRoutes.homeScreenRouteName:(context)=>HomeScreen()
       },
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
-  
+  static const Color primaryLight=Color(0xff5669FF);
+  static const Color primaryDark=Color(0xff101127);
+  static const Color whiteColor=Color(0xffF2FEFF);
 }
